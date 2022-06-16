d1950898b6030407c5ef2910e49acb1d38010da1
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-06-16 16:47:41 +0430

    table component created

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import styles from "styles/HomePage.module.scss";
+import { BiTrash } from "react-icons/bi";
+
+interface User {
+  firstname: string;
+  lastname: string;
+  phone: string;
+  code: string;
+}
+
+// const Form: React.FC<Props> = () => {
+//     return (  );
+// }
+
+interface tableProps {
+  users: User[];
+}
+
+const Table: React.FC<tableProps> = ({ users }) => {
+  return (
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr>
+          <th>نام</th>
+          <th>نام خانوادگی</th>
+          <th>شماره موبایل</th>
+          <th>کد ملی</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {users.map((user) => (
+          <tr key={user.code}>
+            <td>{user.firstname}</td>
+            <td>{user.lastname}</td>
+            <td>{user.phone}</td>
+            <td>{user.code}</td>
+            <td>
+              <button className={styles.btn}>{<BiTrash />}</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const HomePage: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([
+    {
+      firstname: "علی",
+      lastname: "محمودی",
+      phone: "656465545651",
+      code: "464646456",
+    },
+  ]);
+  return (
+    <div className={`container ${styles.container}`}>
+      {users.length ? <Table users={users} /> : ""}
+    </div>
+  );
+};
+
+export default HomePage;
