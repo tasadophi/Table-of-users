4512875d68d20a9f57c56ab83a7b701fff500f67
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-06-17 00:47:29 +0430

    value props added

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -4,16 +4,18 @@ import styles from "styles/Input.module.scss";
 interface Props {
   formik: any;
   name: string;
+  value: string;
   placeholder: string;
   type: string;
 }
 
-const Input: React.FC<Props> = ({ formik, name, placeholder, type }) => {
+const Input: React.FC<Props> = ({ formik, name, value, placeholder, type }) => {
   return (
     <input
       className={styles.input}
       type={type}
       name={name}
+      value={value}
       onBlur={formik.handleBlur}
       onChange={formik.handleChange}
       placeholder={placeholder}
